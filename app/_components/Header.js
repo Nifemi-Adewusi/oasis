216bd580fa8ae6dd8e60d1d599741a89933ade1f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-09-15 16:51:43 +0100

    Update the Header to show on mobile screens

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -3,8 +3,8 @@ import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="border-b border-primary-900 px-8 py-5">
-      <div className="md:flex md:justify-between md:items-center max-w-7xl mx-auto">
+    <header className="md:border-b relative z-10 md:border-primary-900 px-8 py-5">
+      <div className="md:flex md:justify-between md:items-center max-w-7xl mx-auto mt-10 md:mt-0">
         <Logo />
         <Navigation />
       </div>
