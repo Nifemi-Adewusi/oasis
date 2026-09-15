86a0efef60909dd01e9bf6a8ddf946a65fb9f63f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-09-15 16:40:41 +0100

    Update the root layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,15 @@ import Footer from "@/app/_components/Footer";
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// console.log(josefin);
 
 export const metadata = {
   title: {
@@ -15,13 +24,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
+      <body
+        className={`${josefin.className} bg-primary-800 text-primary-100 min-h-screen flex flex-col antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl  mx-auto">{children}</main>
+          {/* <Footer /> */}
+        </div>
       </body>
     </html>
   );
